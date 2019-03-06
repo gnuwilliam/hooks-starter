4989892af084d01e087d77f87f203b7e3d43ef9f
William Oliveira <sudowilliam@gmail.com>
2019-03-06 15:49:22 -0300

    Hooks examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ToggleButton from './Toggle';
 
 const App = () => {
+  // typical useState definition => React Hooks
+  // const [value, setValue] = useState(initialState)
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <ToggleButton />
+      <h3>{name}</h3>
+
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          formSubmit(name, setName);
+        }}
+      >
+        <input
+          type="text"
+          name="myInput"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button type="submit">Submit!</button>
+      </form>
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log(`email sent to ${value}!`);
+  setValue('');
 };
 
 export default App;
---
+++ src/Toggle.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const ToggleButton = () => {
+  const [isToggled, setIsToggled] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+      {isToggled && <h2>Hello!</h2>}
+    </div>
+  );
+};
+
+export default ToggleButton;
